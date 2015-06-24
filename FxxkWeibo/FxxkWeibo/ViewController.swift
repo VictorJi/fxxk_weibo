e387e3e6f5e3f812da87b31fedef3f50b86f02c1
jybn0983 <jybn0983@corp.netease.com>
2015-06-24 20:06:31 +0800

    create

---
+++ FxxkWeibo/FxxkWeibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FxxkWeibo
+//
+//  Created by Victor Ji on 15/6/24.
+//  Copyright (c) 2015年 Victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
